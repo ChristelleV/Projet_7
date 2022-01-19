8d70de8dee95c125c00ed6d5a18f59162d027b5e
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-19 11:19:16 +0100

    Add files via upload

---
+++ API.py
@@ -0,0 +1,11 @@
+import mlflow
+import subprocess
+
+
+subprocess.Popen(["mlflow", "models", "serve", "-m", "runs:/mlflow_model/https://github.com/Edsondev21/Projet_7/tree/main/mlflow_model"])
+
+
+
+   # "mlflow models serve -m https://github.com/Edsondev21/Projet_7/tree/main/mlflow_model", shell = True, bufsize = 0, stdout = subprocess.PIPE)
+  #
+#mlflow models serve -m mlflow_model/
