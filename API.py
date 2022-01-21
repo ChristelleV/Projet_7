4a87d7b866f50263cb5634c50ce38116eaca91e3
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-21 14:36:51 +0100

    Update API.py
    remplacer uri https par s3

--- API.py
+++ API.py
@@ -2,7 +2,7 @@ import mlflow
 import subprocess
 
 
-subprocess.Popen(["mlflow", "models", "serve", "-m", "s3.console.aws.amazon.com/s3/buckets/mlflowmodel?region=eu-west-3&prefix=mlflow_model/&showversions=false"])
+subprocess.Popen(["mlflow", "models", "serve", "-m", "s3://mlflowmodel/mlflow_model/"])
 
 #subprocess.Popen(["mlflow", "models", "serve", "-m", "https://github.com/Edsondev21/Projet_7/tree/main/mlflow_model"])
 
