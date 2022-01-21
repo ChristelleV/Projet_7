0ffa1018923e9fa2f8803ce977ff2c8930c7f131
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-21 22:54:00 +0100

    Update API.py
    remplacer les guillemets

--- API.py
+++ API.py
@@ -2,8 +2,8 @@ import mlflow
 import subprocess
 import os
 
-os.environ[“AWS_ACCESS_KEY_ID”]=st.secrets[“AWS_ACCESS_KEY_ID”] 
-os.environ[“AWS_SECRET_ACCESS_KEY ”]=st.secrets[“AWS_SECRET_ACCESS_KEY”] 
+os.environ["AWS_ACCESS_KEY_ID"]=st.secrets["AWS_ACCESS_KEY_ID"] 
+os.environ["AWS_SECRET_ACCESS_KEY"]=st.secrets["AWS_SECRET_ACCESS_KEY"] 
 
 subprocess.Popen(["mlflow", "models", "serve", "-m", "s3://mlflowmodel/mlflow_model/"])
 
