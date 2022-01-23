d3eee77685a7814a0f8593c2a884abdd3aa8c238
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-23 22:22:56 +0100

    Update API.py

--- API.py
+++ API.py
@@ -6,5 +6,6 @@ import os
 os.environ["AWS_ACCESS_KEY_ID"]=st.secrets["AWS_ACCESS_KEY_ID"] 
 os.environ["AWS_SECRET_ACCESS_KEY"]=st.secrets["AWS_SECRET_ACCESS_KEY"] 
 
-subprocess.Popen(["mlflow", "models", "serve", "-m", "s3://mlflowmodel/mlflow_model/"])
+#subprocess.Popen(["mlflow", "models", "serve", "-m", "s3://mlflowmodel/mlflow_model/"])
 
+mlflow run https://github.com/mlflow/mlflow-example.git -P alpha=5.0
