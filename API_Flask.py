7eba943950af50faaec0ef1011ae3292b8f7c217
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-24 15:23:23 +0100

    Update API_Flask.py
    supprimer debug=True

--- API_Flask.py
+++ API_Flask.py
@@ -17,4 +17,4 @@ def makecalc():
 if __name__ == '__main__':
     modelfile = 'final_prediction.pickle'
     model = p.load(open(modelfile, 'rb'))
-    app.run(debug=True, host='127.0.0.1')
+    app.run(host='127.0.0.1')
