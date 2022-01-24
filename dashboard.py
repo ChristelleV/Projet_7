204a1ae3c27f84e974dda49af94417c260cb9251
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-24 15:43:51 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ import requests
 import json
 import App1
 import App2
-import API
+import API_Flask
 from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
 import pickle as p
@@ -27,7 +27,7 @@ page = PAGES[selection]
 page.app()
 
 
-
+#####################################
 
 
 # Préparation des données
@@ -40,38 +40,8 @@ Y = df['TARGET']
 X = pd.read_csv("https://raw.githubusercontent.com/Edsondev21/Projet_7/main/X_scaled.csv")
 X.drop(['Unnamed: 0'], axis=1, inplace=True)
 
-#####################################
-
-# Fonction de prediction via l'API
-def request_prediction(data):
-    headers = {"Content-Type": "application/json"}
-    #data_json = {'data': data}
-    response = requests.request(
-        method='POST', headers=headers, url='http://127.0.0.1:5000/invocations', json=data)
-    if response.status_code != 200:
-        raise Exception(
-            "Request failed with status {}, {}".format(response.status_code, response.text))
-    return response
-
-def request_pred(data):
-    url = "http://127.0.0.1:5000/invocations"
-    dat = data.to_json(orient='split')
-# headers de la requête
-    headers = {'Content-type': 'application/json'}
-# la requête
-    r = requests.post("http://127.0.0.1:5000/invocations", data=json.dumps(dat), headers=headers)
-    if r.status_code != 200:
-        raise Exception(
-            "Request failed with status {}, {}".format(r.status_code, r.text))
-
-    return r
-
-mlflow_url = 'http://127.0.0.1:5000/invocations'
 
 
-
-
-############################################################################
 
 
 
