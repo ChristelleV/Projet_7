2a393ba6f448aee977c4a8b96c8885d7f7b655ba
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-26 14:28:27 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,6 +17,20 @@ import os
 
 ###################################################################################
 
+app = Flask(__name__)
+
+
+@app.route('/api/', methods=['POST'])
+def makecalc():
+    data = request.get_json()
+    prediction = np.array2string(model.predict(data))
+
+    return jsonify(prediction)
+
+if __name__ == '__main__':
+    modelfile = 'final_prediction.pickle'
+    model = p.load(open(modelfile, 'rb'))
+    app.run(debug=True, host='127.0.0.1')
 
 #############################################################################
 
