ca757b0b526a2ad7ea0fba8fa07ff98496c5269d
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-28 22:26:11 +0100

    Update API_Flask.py

--- API_Flask.py
+++ API_Flask.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
+import pandas as pd
 import pickle as p
 import json
+
 
 app = Flask(__name__)
 
@@ -9,6 +11,8 @@ app = Flask(__name__)
 @app.route('/api/', methods=['POST'])
 def makecalc():
     data = request.get_json()
+    print(data)
+    print(type(data))
     data_df = pd.read_json(data, orient='split')
     prediction = np.array2string(model.predict(data_df.to_numpy().reshape(1, -1)))
     return jsonify(prediction)
