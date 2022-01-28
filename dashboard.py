cedcf67d947d3e6a53eab569fecde257720ba3a4
Sona Sukiasyan <93388055+Edsondev21@users.noreply.github.com>
2022-01-28 15:37:05 +0100

    Update dashboard.py
    Supprimer les données inutiles

--- dashboard.py
+++ dashboard.py
@@ -2,12 +2,10 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import requests
 import json
 import App1
 import App2
-#import API_Flask
 from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
 import pickle as p
@@ -15,10 +13,7 @@ import subprocess
 from subprocess import Popen, PIPE
 import os
 
-###################################################################################
 
-
-#############################################################################
 
 st.sidebar.title('Tableau de bord pour prédiction de crédit ')
 st.sidebar.subheader("Navigation")
@@ -29,36 +24,3 @@ PAGES = {"Vue Générale": App1,
 selection = st.sidebar.radio("Aller à", list(PAGES.keys()))
 page = PAGES[selection]
 page.app()
-
-
-
-#####################################
-
-
-# Préparation des données
-df = pd.read_csv('https://raw.githubusercontent.com/Edsondev21/Projet_7/main/data2.csv')    
-df.drop(['Unnamed: 0'], axis=1, inplace=True)
-
-dt = df.drop(['TARGET'], axis=1)
-Y = df['TARGET']
-
-X = pd.read_csv("https://raw.githubusercontent.com/Edsondev21/Projet_7/main/X_scaled.csv")
-X.drop(['Unnamed: 0'], axis=1, inplace=True)
-
-
-
-app = Flask(__name__)
-
-
-@app.route('/api/', methods=['POST'])
-def makecalc():
-    data = request.get_json()
-    prediction = np.array2string(model.predict(data))
-    return jsonify(prediction)
-
-
-if __name__ == '__main__':
-    modelfile = 'final_prediction.pickle'
-    model = p.load(open(modelfile, 'rb'))
-    app.run(host='127.0.0.1') --no-reload
-
